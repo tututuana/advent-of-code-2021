6fc01ab15a8945c3b280b5ddde6ebf0c08568fb1
tututuana <51187395+tututuana@users.noreply.github.com>
2021-12-03 12:09:58 +0300

    Create main.py

---
+++ day3/main.py
@@ -0,0 +1,60 @@
+def CountBits(set, bit):
+	one = 0
+	zero = 0
+	for num in set:
+		if (num & (1 << bit)) != 0: one += 1
+		else: zero += 1
+	return (one, zero)
+
+def Day03(input):
+	bits = len(input[0])
+	bitsets = [[0,0] for i in range(bits)]
+	nums = []
+
+	for line in input:
+		if line == "": continue
+		num = int(line, 2)
+		for i in range(bits):
+			if ((num & (1 << i)) != 0): bitsets[i][0] += 1
+			else: bitsets[i][1] += 1
+		nums.append(num)
+
+	gamma = 0
+	epsilon = 0
+	for i in range(bits):
+		gamma |= (bitsets[i][0] >= bitsets[i][1]) and (1 << i) or 0
+		epsilon |= (bitsets[i][0] < bitsets[i][1]) and (1 << i) or 0
+
+	print(f"Part 1: {epsilon * gamma}")
+
+	current = nums
+	oxygen = 0
+	for i in range(bits-1, -1, -1):
+		bitCount = CountBits(current, i)
+		if bitCount[0] >= bitCount[1]:
+			current = [num for num in current if (num & (1 << i)) != 0]
+		else:
+			current = [num for num in current if (num & (1 << i)) == 0]
+		if len(current) == 1:
+			oxygen = current[0]
+			break
+
+	current = nums
+	co2 = 0
+	for i in range(bits-1, -1, -1):
+		bitCount = CountBits(current, i)
+		if bitCount[0] < bitCount[1]:
+			current = [num for num in current if (num & (1 << i)) != 0]
+		else:
+			current = [num for num in current if (num & (1 << i)) == 0]
+		if len(current) == 1:
+			co2 = current[0]
+			break
+
+	print(f"Part 2: {oxygen * co2}")
+
+
+if __name__ == "__main__":
+    with open("input.txt", "r") as f:
+        input = f.read().splitlines()
+    Day03(input)
